5257a9ec4a7d427e388e8fa90e60e46b96b8842e
mathieukn229 <mathieukn229@gmail.com>
2026-07-25 19:31:29 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+// MKAI Platform V2
 
+// Bouton commencer conversation
+const startButton = document.querySelector(".primary-btn");
+
+if(startButton){
+    startButton.addEventListener("click", () => {
+        alert("Bienvenue dans MKAI 💙");
+    });
+}
+
+
+// Animation des cartes outils
+
+const cards = document.querySelectorAll(".tool-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        const title = card.querySelector("h4").innerText;
+
+        alert("Ouverture de : " + title);
+
+    });
+
+});
